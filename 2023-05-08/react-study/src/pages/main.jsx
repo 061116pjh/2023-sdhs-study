31466d6b8029d287905e3483989d74f5bb61cb81
pjhpkm0727 <061116pjh@naver.com>
2023-05-08 16:07:35 +0900

    feat Router

---
+++ 2023-05-08/react-study/src/pages/main.jsx
@@ -0,0 +1,12 @@
+import Button from '../components/button';
+
+function Main(){
+  return(
+    <>
+      <div>Root Page.</div>
+      <Button color="black" size="20">만들기</Button>
+    </>
+  );
+}
+
+export default Main;
