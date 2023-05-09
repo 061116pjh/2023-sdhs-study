884174e638d692266a8a9e875f4866599a5f1fb4
pjhpkm0727 <061116pjh@naver.com>
2023-05-09 11:18:52 +0900

    feat: add todos router

--- 2023-05-08/react-study/src/pages/router.jsx
+++ 2023-05-08/react-study/src/pages/router.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './main/index';
+import Todos from './todos/index';
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Main />} />
+                <Route path="/todos" element={<Todos />}></Route>
             </Routes>
         </BrowserRouter>
     );
