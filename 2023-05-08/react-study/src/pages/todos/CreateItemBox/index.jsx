ed01caf0ff2c0e39c8f45b261a5f6feac07de6fa
pjhpkm0727 <061116pjh@naver.com>
2023-05-09 11:21:50 +0900

    feat: onChange input value

---
+++ 2023-05-08/react-study/src/pages/todos/CreateItemBox/index.jsx
@@ -0,0 +1,15 @@
+import Button from '../../../components/button/index';
+import * as S from './styled';
+
+function CreateItemBox({onChange}){
+  const handleInputValue = (e) => {
+    onChange(e.target.value);
+  }
+  return(
+    <S.InputBox>
+        <S.Input placeholder='To do를 입력해주세요.' onChange={handleInputValue} />
+        <Button>등록</Button>
+    </S.InputBox>
+  );
+}
+export default CreateItemBox;
