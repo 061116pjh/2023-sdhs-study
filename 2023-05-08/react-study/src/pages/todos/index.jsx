0cdad21b203d32ddae6a51dcfd483b81dd07926d
pjhpkm0727 <061116pjh@naver.com>
2023-05-09 13:03:25 +0900

    feat: createTodo

--- 2023-05-08/react-study/src/pages/todos/index.jsx
+++ 2023-05-08/react-study/src/pages/todos/index.jsx
@@ -5,14 +5,18 @@ import ItemList from './itemLIst/indesx'
 
 function Todos(){
   const [todoName, setTodoName] = useState("");
-  const [items, setItems] = useState([]);
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = () => {
+    setTodos(prevState => [...prevState, {id: prevState.length, name: todoName}]);
+  }
 
   return (
     <S.Container>
       <S.Title>To do List</S.Title>
       {todoName}
-      <CreateItemBox onChange={setTodoName}/>
-      <ItemList />
+      <CreateItemBox onChange={setTodoName} createTodo={createTodo}/>
+      <ItemList todos={todos} />
     </S.Container>
   );
 }
