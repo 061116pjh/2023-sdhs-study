7b2122e5ae7c6b2dc342fc35d311d31db27e9b32
pjhpkm0727 <061116pjh@naver.com>
2023-05-09 11:19:28 +0900

    feat: link to todos

--- 2023-05-08/react-study/src/pages/main/index.jsx
+++ 2023-05-08/react-study/src/pages/main/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Button from '../../components/button';
 import * as S from './styled';
 
@@ -6,7 +7,9 @@ function Main(){
     <S.Container>
       <S.MainWapper>
         <div>Root Page.</div>
-        <Button>만들기</Button>
+        <Button>
+          <Link to="todos">만들기</Link>
+        </Button>
       </S.MainWapper>
     </S.Container>
   );
