f8c32f3d300eff00d7a43ee5fae1866609a653ee
pjhpkm0727 <061116pjh@naver.com>
2023-05-10 17:02:49 +0900

    feat: delete todo

--- 2023-05-08/react-study/src/pages/todos/index.jsx
+++ 2023-05-08/react-study/src/pages/todos/index.jsx
@@ -1,23 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import * as S from './styled';
 import CreateItemBox from './CreateItemBox/index';
-import ItemList from './itemLIst/indesx'
+import ItemList from './itemLIst/indesx';
+import { v4 as uuidv4 } from 'uuid';
 
 function Todos(){
   const [todoName, setTodoName] = useState("");
   const [todos, setTodos] = useState([]);
 
   const createTodo = () => {
-    setTodos(prevState => [...prevState, {id: prevState.length, name: todoName}]);
+    if(!todoName.trim()) return;
+    
+    setTodoName('');
+    setTodos(prevState => [...prevState, {id: uuidv4(), name: todoName}]);
   }
 
+  const deleteTodo = (id) => {
+    const findIndex = todos.findIndex(v => v.id === id);
+    setTodos(prevState => {
+      const tempArr = [...prevState];
+      tempArr.splice(findIndex, 1);
+      return tempArr;
+    });
+  }
+
+  // useEffect(() => {
+  //   localStorage.setItem.toString("todo", JSON.stringify(todos));
+  // }, [todos]);
+
   return (
-    <S.Container>
+    <S.Body>
+      <S.Container>
       <S.Title>To do List</S.Title>
-      {todoName}
-      <CreateItemBox onChange={setTodoName} createTodo={createTodo}/>
-      <ItemList todos={todos} />
-    </S.Container>
+      <CreateItemBox value={todoName} onChange={setTodoName} createTodo={createTodo} />
+      <ItemList todos={todos} deleteTodo={deleteTodo} />
+      </S.Container>
+    </S.Body>
   );
 }
 
