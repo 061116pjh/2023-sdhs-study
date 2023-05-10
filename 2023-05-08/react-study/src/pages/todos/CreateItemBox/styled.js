f848d39d057b1c8de23decd73ebb2571b2c4657b
pjhpkm0727 <061116pjh@naver.com>
2023-05-10 17:06:42 +0900

    feat: input style

--- 2023-05-08/react-study/src/pages/todos/CreateItemBox/styled.js
+++ 2023-05-08/react-study/src/pages/todos/CreateItemBox/styled.js
@@ -4,18 +4,22 @@ import { Button } from '../../../components/button/styled';
 
 export const InputBox = styled.div`
   display: flex;
-  width: 100%;
+  box-shadow: 2px 2px 10px #eee;
   ${Button}{
-    flex-grow: 1;
-    padding: 4px 8px;
+    padding: 10px 20px;
     border: none;
     cursor: pointer;
-    flex-shrink: 0;
+    background: #333;
+    color: #fff;
+    font-weight: 600;
+    font-size: 15px;
   }
 `;
 
 export const Input = styled.input`
-  width: 100%;
-  flex-shrink: 0;
-  padding: 4px;
+  width: 400px;
+  height: 50px;
+  padding: 0 15px;
+  border: none;
+  outline: none;
 `;
