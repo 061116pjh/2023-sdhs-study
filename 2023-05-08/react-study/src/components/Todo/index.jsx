25550fbb83acc9ba6753c50d89b0b86c541a3725
pjhpkm0727 <061116pjh@naver.com>
2023-05-10 17:01:26 +0900

    feat: delete

--- 2023-05-08/react-study/src/components/Todo/index.jsx
+++ 2023-05-08/react-study/src/components/Todo/index.jsx
@@ -2,11 +2,14 @@ import * as S from './styled';
 import Button from '../button/index';
 
 function Todo(props){
+  const { deleteTodo } = props;
   return(
     <S.Container>
       <S.Title>{props.children}</S.Title>
-      <Button>중요</Button>
-      <Button>삭제</Button>
+      <S.Btns>
+        <Button>중요</Button>
+        <Button onClick={deleteTodo}>삭제</Button>
+      </S.Btns>
     </S.Container>
   );
 }
