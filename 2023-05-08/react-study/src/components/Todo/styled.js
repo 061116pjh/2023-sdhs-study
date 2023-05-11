122b0dafd6f40da5dcff4628baf540bde0f92657
pjhpkm0727 <061116pjh@naver.com>
2023-05-11 10:07:59 +0900

    DELETE: delete Btns css

--- 2023-05-08/react-study/src/components/Todo/styled.js
+++ 2023-05-08/react-study/src/components/Todo/styled.js
@@ -12,31 +12,10 @@ export const Container = styled.li`
   padding: 0 15px;
   border: solid 1px #eee;
   background: #fff;
-`;
-
-export const Btns = styled.div`
-  display: flex;
-  height: 100%;
-  align-items: center;
-  width: 120px;
-  justify-content: space-between;
   >${Button}{
     padding: 8px 15px;
     border: none;
     cursor: pointer;
-    flex-shrink: 0;
-  }
-  >${Button}:nth-child(1){
-    background: #333;
-    color: #fff;
-  }
-  >${Button}:nth-child(1):hover{
-    border: solid 1px #333;
-    color: #333;
-    background: #fff;
-    font-weight: 600;
-  }
-  >${Button}:nth-child(2){
     background: red;
     color: #fff;
   }
