1e8b502c7994649f5cf5074a85c505732b44e123
pjhpkm0727 <061116pjh@naver.com>
2023-05-11 10:08:21 +0900

    FEAT: input enter

--- 2023-05-08/react-study/src/pages/todos/CreateItemBox/index.jsx
+++ 2023-05-08/react-study/src/pages/todos/CreateItemBox/index.jsx
@@ -5,9 +5,12 @@ function CreateItemBox({value, onChange, createTodo}){
   const handleInputValue = (e) => {
     onChange(e.target.value)
   }
+  const enterInput= (e) => {
+    if(e.code === "Enter") createTodo();
+  }
   return(
     <S.InputBox>
-        <S.Input value={value} placeholder='To do를 입력해주세요.' onChange={handleInputValue} autoFocus />
+        <S.Input value={value} placeholder='To do를 입력해주세요.' onKeyUp={enterInput} onChange={handleInputValue} autoFocus />
         <Button onClick={createTodo}>등록</Button>
     </S.InputBox>
   );
