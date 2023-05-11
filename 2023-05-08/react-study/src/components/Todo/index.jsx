641c73634c39c8ff21cdb1c58bc87bab4cdab97d
pjhpkm0727 <061116pjh@naver.com>
2023-05-11 10:08:25 +0900

    DELETE: delete important Btn

--- 2023-05-08/react-study/src/components/Todo/index.jsx
+++ 2023-05-08/react-study/src/components/Todo/index.jsx
@@ -6,10 +6,7 @@ function Todo(props){
   return(
     <S.Container>
       <S.Title>{props.children}</S.Title>
-      <S.Btns>
-        <Button>중요</Button>
-        <Button onClick={deleteTodo}>삭제</Button>
-      </S.Btns>
+      <Button onClick={deleteTodo}>삭제</Button>
     </S.Container>
   );
 }
