65a75783e55ca76b17b28e5bf0c9a2a554d58001
pjhpkm0727 <061116pjh@naver.com>
2023-05-12 10:11:00 +0900

    FEAT: searchInput style

---
+++ 2023-05-08/react-study/src/components/SerchInput/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const serchInput = styled.input`
+  width: 300px;
+  height: 50px;
+  padding: 0 20px;
+  border: none;
+  outline: none;
+  border-radius: 15px;
+`;
