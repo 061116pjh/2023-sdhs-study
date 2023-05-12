510a3d7c789f7c0230f614abc460bc99af9a2dc5
pjhpkm0727 <061116pjh@naver.com>
2023-05-12 13:43:55 +0900

    FEAT: selected delete

--- 2023-05-08/react-study/src/pages/todos/index.jsx
+++ 2023-05-08/react-study/src/pages/todos/index.jsx
@@ -3,12 +3,14 @@ import * as S from './styled';
 import CreateItemBox from './CreateItemBox/index';
 import SerchInput from '../../components/SerchInput/index';
 import ItemList from './itemLIst/indesx';
+import Button from '../../components/button/index';
 import { v4 as uuidv4 } from 'uuid';
 
 function Todos(){
   const [todoName, setTodoName] = useState("");
   const [todos, setTodos] = useState([]);
   const [serchValue, setSerchValue] = useState("");
+  const [selectedTodoIds, setSelectdTodoIds] = useState([]);
 
   const createTodo = () => {
     if(!todoName.trim()) return;
@@ -20,6 +22,12 @@ function Todos(){
   const deleteTodo = (id) => {
     const filterTodos = todos.filter(v => v.id !== id);
     setTodos(filterTodos);
+  }
+
+  const deleteSelectedTodos = () => {
+    setTodos(prevState => {
+      return prevState.filter(todo => !selectedTodoIds.includes(todo.id));
+    });
   }
 
   useEffect(() => {
@@ -46,7 +54,8 @@ function Todos(){
       <S.Title>To do List</S.Title>
       <SerchInput onChange={(value) => {setSerchValue(value)}} />
       <CreateItemBox value={todoName} onChange={setTodoName} createTodo={createTodo} />
-      <ItemList todos={todos} serchValue={serchValue} deleteTodo={deleteTodo} />
+      <Button onClick={deleteSelectedTodos}>선택 된 To do 삭제</Button>
+      <ItemList todos={todos} serchValue={serchValue} deleteTodo={deleteTodo} setSelectdTodoIds={setSelectdTodoIds} />
       </S.Container>
     </S.Body>
   );
