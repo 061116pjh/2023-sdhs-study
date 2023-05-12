0c329f354909300a4c1711c44aff919f01bf80e2
pjhpkm0727 <061116pjh@naver.com>
2023-05-12 13:43:06 +0900

    FEAT: add checkbox

--- 2023-05-08/react-study/src/components/Todo/index.jsx
+++ 2023-05-08/react-study/src/components/Todo/index.jsx
@@ -1,10 +1,12 @@
 import * as S from './styled';
 import Button from '../button/index';
+import Checkbox from '../Checkbox/index';
 
 function Todo(props){
-  const { deleteTodo } = props;
+  const { deleteTodo, handleSelected } = props;
   return(
     <S.Container>
+      <Checkbox onChange={handleSelected} />
       <S.Title>{props.children}</S.Title>
       <Button onClick={deleteTodo}>삭제</Button>
     </S.Container>
