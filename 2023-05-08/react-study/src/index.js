6e7a0a41d877a25a6a9629dc751ebe4c9f089a63
pjhpkm0727 <061116pjh@naver.com>
2023-05-15 15:27:39 +0900

    FEAT: provider 추가해서 store 사용

--- 2023-05-08/react-study/src/index.js
+++ 2023-05-08/react-study/src/index.js
@@ -1,6 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import Router from './pages/router';
+import { Provider } from 'react-redux';
+import { store } from './reduce/index';
 import './reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Router />);
+root.render(
+<Provider store={store}>
+  <Router />
+</Provider>
+);
