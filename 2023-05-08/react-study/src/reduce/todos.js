ab44438b793547bb4e9d72e5c182db1d2fde837f
pjhpkm0727 <061116pjh@naver.com>
2023-05-15 15:28:21 +0900

    FEAT: add function

--- 2023-05-08/react-study/src/reduce/todos.js
+++ 2023-05-08/react-study/src/reduce/todos.js
@@ -1,18 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  todos: [],
+}
+
 const todosSlice = createSlice({
-  name: todos,
-  initialState: [],
+  name: "todos",
+  initialState,
   reducers: {
-    createTodo: (state, action) => {
-
-    },deleteTodo: (state, action) => {
-
-    },deleteSelectedTodos: (state, action) => {
-
+    handleCreateTodo: (state, action) => {
+      const {payload} = action;
+      //payload = {id: uuidv4(), name: todoName};
+      state.todos = [...state.todos, payload];
+      // state.todos = [{id: '1', name: 'test1' }, {}, {}, {id: uuidv4(), name: todoName}]
+    },handleDeleteTodo: (state, action) => {
+      const {payload} = action;
+      const filterTodos = state.todos.filter(v => v.id !== payload);
+      state.todos = filterTodos;
+    },handleDeleteSelectedTodos: (state, action) => {
+      const {payload} = action;
+      const filterTodos = state.todos.filter(todo => !payload.selectedTodoIds.includes(todo.id));
+      state.todos = filterTodos;
     },
   },
 });
 
-export const { createTodo, deleteTodo, deleteSelectedTodos} = todosSlice.actions;
+export const { handleCreateTodo, handleDeleteTodo, handleDeleteSelectedTodos} = todosSlice.actions;
 export default todosSlice.reducer;
