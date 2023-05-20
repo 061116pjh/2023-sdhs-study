4b3285e6ec0e4967cfb62cef675fdc66d90b5103
pjhpkm0727 <061116pjh@naver.com>
2023-05-20 17:13:19 +0900

    FEAT: Login style

---
+++ login/src/pages/login/styled.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Form = styled.form`
+  width: 400px;
+  height: 400px; 
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: solid 1px #eee;
+  justify-content: space-around;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px;
+`;
+
+export const Title = styled.h2``;
