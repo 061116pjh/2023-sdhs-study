bf7b35927147eb7d04a1f3b225c154c780bd4017
pjhpkm0727 <061116pjh@naver.com>
2023-05-20 17:15:06 +0900

    FEAT: Button component style

---
+++ login/src/component/button/styled.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  width: 250px;
+  height: 50px;
+  line-height: 50px;
+  text-align: center;
+  border-radius: 10px;
+  background: rgb(45, 140, 255);
+  border: none;
+  cursor: pointer;
+  color: #fff;
+  font-weight: 600;
+  font-size: 17px;
+`;
