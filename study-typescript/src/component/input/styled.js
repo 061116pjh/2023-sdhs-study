4d58d399852c581ce8ddeb4483be13a199c26bb0
pjhpkm0727 <061116pjh@naver.com>
2023-05-25 16:26:32 +0900

    FEAT: Input component style

---
+++ study-typescript/src/component/input/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 500px;
+  height: 50px;
+  border-radius: 5px;
+  outline: none;
+  border: solid 1px #eee;
+  padding: 0 15px;
+`;
