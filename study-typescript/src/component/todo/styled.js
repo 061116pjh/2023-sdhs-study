6e63c0be941bae767d05e5b1878d9cc499dfb918
pjhpkm0727 <061116pjh@naver.com>
2023-05-25 16:27:42 +0900

    FEAT: List style

---
+++ study-typescript/src/component/todo/styled.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const List = styled.li`
+  width: 100%;
+  min-height: 50px;
+  display: flex;
+  border-radius: 5px;
+  justify-content: space-around;
+  border: solid 1px #eee;
+  align-items: center;
+`;
+
+export const Btns = styled.div`
+  width: 150px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Text = styled.p`
+display: flex;
+justify-content: center;
+align-items: center;
+  min-height: 40px; 
+  width: 300px;
+`;
